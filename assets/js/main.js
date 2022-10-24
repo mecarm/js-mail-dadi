4813a7e2f88177bc7c1f7359da9d22af18f0f601
mecarm <melecarmine99@gmail.com>
2022-10-24 16:19:08 +0200

    Imposto gioco dati su un'altra pagina html

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,26 @@
     -se non è presente non può accedere
     -se è presente vai avanti
 3-dopo controllo far comparire il gioco.
+4- creare una funzione che genere un numero da 1 a 6
+    -Il primo per il player
+    -il secondo per il bot
+
 */
 
 const giocatori = ["mecar99@gmail.com", 'pippo12@gmail.com', 'baudo13@gmail.com'];
 
-
 function valida(){
     let email = document.getElementById('email').value;
     if (giocatori.includes(email) ){
-        console.log('Bentornato, ora puoi giocare')
+        console.log('Bentornato, ora puoi giocare');
+        document.getElementById('bentornato').innerHTML = `Bentornato ${email}`;
+        document.getElementById('play').className = 'd-show';
+
     } else {
         console.log('Non sei iscritto')
     }
 }
+
+
 
 
